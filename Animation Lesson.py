fc47e9f408d7db3bf17d7985267bc267ccb942ad
ZinhNguyen <vannhinh91@gmail.com>
2022-02-05 14:21:05 +0700

    use class for car

--- Animation Lesson.py
+++ Animation Lesson.py
@@ -20,25 +20,37 @@ fpsClock = pygame.time.Clock()
 DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH,WINDOWHEIGHT))
 pygame.display.set_caption('Animation')
 
-### Create Surface for car ###
-# Set x coordinate
-car_x = 0
-carSurface = pygame.Surface((120, 50), SRCALPHA)
-pygame.draw.polygon(carSurface, RED, ((15, 0), (65, 0), (85, 15), (110, 15), (120, 20), (120, 35), (115, 40), (0, 40), (0, 15)))
-pygame.draw.circle(carSurface, BLUE, (15, 40), 10)
-pygame.draw.circle(carSurface, BLUE, (85, 40), 10)
+### Create Class for car ###
+class Car():
+    def __init__(self):
+        # Set x coordinate
+        self.x = 0
 
+        # Set surface for car
+        self.surface = pygame.Surface((120, 50), SRCALPHA)
+        pygame.draw.polygon(self.surface, RED, ((15, 0), (65, 0), (85, 15), (110, 15), (120, 20), (120, 35), (115, 40), (0, 40), (0, 15)))
+        pygame.draw.circle(self.surface, BLUE, (15, 40), 10)
+        pygame.draw.circle(self.surface, BLUE, (85, 40), 10)
+
+    # Draw car
+    def draw(self):
+        DISPLAYSURF.blit(self.surface, (self.x, 100))
+
+    # Update car
+    def update(self):
+        self.x +=2
+        if self.x + 120 > WINDOWWIDTH:
+            self.x = WINDOWWIDTH - 120
+
+car = Car()
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
     DISPLAYSURF.fill(WHITE)
-    DISPLAYSURF.blit(carSurface, (car_x, 100))
-
-    ### Change position of car ###
-    car_x +=5
-    if car_x + 120 > WINDOWWIDTH:
-        car_x = WINDOWWIDTH - 120
+    print(car.x)
+    car.draw()
+    car.update()
     pygame.display.update()
     fpsClock.tick(FPS)
