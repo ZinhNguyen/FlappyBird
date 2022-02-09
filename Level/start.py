f9cb2768b8682a1b453894f8e7c332cfd7a4650b
ZinhNguyen <67100275+ZinhNguyen@users.noreply.github.com>
2022-02-09 17:29:19 +0700

    Add event bird_flap
    Add Event bird_flap

--- Level/start.py
+++ Level/start.py
@@ -6,6 +6,7 @@ from Image import bird as b
 from Image import column as co
 from Function import checkGameOver as go
 from Sound import start as st
+from Function import birdAnimation as ba
 
 
 
@@ -38,9 +39,16 @@ def Start(bird, columns, score):
                 if event.key == K_SPACE:
                     mouseClick = True
                     st.flap_sound.play()
+            if event.type == b.BIRD_FLAP:
+                if b.BIRD_INDEX < 2:
+                    b.BIRD_INDEX +=1
+                else:
+                    b.BIRD_INDEX =0
+                b.BIRD = ba.bird_animation()
         if go.isGameOver(bird, columns) == True:
             st.die_sound.play()
             return
+
         SCREEN.blit(BG, (0, 0))
         columns.draw()
         columns.update()
