255c57384d8af20185fb399aa8577a2f1fb99916
ZinhNguyen <67100275+ZinhNguyen@users.noreply.github.com>
2022-02-11 17:26:33 +0700

    Update background.py
    Update Logo and Arrow

--- Image/background.py
+++ Image/background.py
@@ -13,6 +13,15 @@ BACKGROUND = pygame.transform.scale(BACKGROUND, (WINDOWWIDTH, WINDOWHEIGHT))
 M_SCREEN = pygame.image.load('Sources/images/floor.png')
 M_SCREEN = pygame.transform.scale2x(M_SCREEN)
 
+# Add Arrow for Background
+ARROW = pygame.image.load('Sources/images/arrow1.png')
+ARROW = pygame.transform.scale(ARROW, (60, 50))
+
+# Add Logo for Background
+LOGO = pygame.image.load('Sources/images/logo.png')
+LOGO = pygame.transform.scale(LOGO, (350, 80))
+LOGO_WIDTH = LOGO.get_width()
+
 def draw_screen(M_SCREEN_X_POS):
     SCREEN.blit(M_SCREEN, (M_SCREEN_X_POS, 600))
     SCREEN.blit(M_SCREEN, (M_SCREEN_X_POS + WINDOWWIDTH + 25, 600))
