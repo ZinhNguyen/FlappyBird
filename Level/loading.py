237e43b730b3992d88a9c375509435bba55b5a0f
ZinhNguyen <67100275+ZinhNguyen@users.noreply.github.com>
2022-02-11 17:39:31 +0700

    Update loading.py
    Add KeyDown to start

--- Level/loading.py
+++ Level/loading.py
@@ -4,6 +4,7 @@ from Image import screen as sc
 from Image import background as bg
 from Image import column as co
 from Function import flyLoading as fl
+from Sound import loading as st
 
 BG = bg.BACKGROUND
 SCREEN = sc.SCREEN
@@ -20,24 +21,30 @@ def Loading(bird):
     # headingSuface = font.render('FLAPPY BIRD', True, (255, 0, 0))
     # headingSize = headingSuface.get_size()
     #
-    # font = pygame.font.SysFont('consolas', 20)
-    # commentSuface = font.render('Click to start', True, (0, 0, 0))
-    # commentSize = commentSuface.get_size()
+    font = pygame.font.SysFont('consolas', 20)
+    commentSuface = font.render('Click or use "Space" to start', True, (0, 0, 0))
+    commentSize = commentSuface.get_size()
     up = True
+    st.battle_sound.play()
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
             if event.type == MOUSEBUTTONDOWN:
+                st.battle_sound.stop()
                 return
+            if event.type == KEYDOWN:
+                if event.key == K_SPACE:
+                    st.battle_sound.stop()
+                    return
 
         SCREEN.blit(BG, (0, 0))
         # make bird fly on loading
         bird.y, up = fl.flying(up, bird.y, 5)
         bird.draw()
         # SCREEN.blit(headingSuface, (int((WINDOWWIDTH - headingSize[0] ) /2), 100))
-        # SCREEN.blit(commentSuface, (int((WINDOWWIDTH - commentSize[0] ) /2), 500))
+        SCREEN.blit(commentSuface, (int((WINDOWWIDTH - commentSize[0])/2), 500))
         M_SCREEN_X_POS -= SCREEN_SPEED
         if M_SCREEN_X_POS <= -WINDOWWIDTH:
             M_SCREEN_X_POS = 0
