88d9bf0cdba599cbfca44ebf32db3c5976815534
ZinhNguyen <67100275+ZinhNguyen@users.noreply.github.com>
2022-02-11 17:40:28 +0700

    Update main.py
    Add more Birds

--- main.py
+++ main.py
@@ -5,22 +5,32 @@ from Image import column as cl
 from Level import loading as lo
 from Level import start as st
 from Level import gameover as go
+from Level import characterChoice as cc
 from Function import calculateScore as cs
 
 pygame.init()
 
 
 def main():
-    bird = b.Bird()
+    blueBird = b.BlueBird()
+    brownBird = b.BrownBird()
+    defaultBird = b.Bird()
+    bird = defaultBird
     columns = cl.Columns()
     score = cs.Score()
     record = 0
     while True:
+        temp = cc.CharacterChoice(blueBird, defaultBird, brownBird)
+        if temp == 1:
+            bird = blueBird
+        elif temp == 2:
+            bird = brownBird
+        else:
+            bird = defaultBird
         lo.Loading(bird)
         st.Start(bird, columns, score)
         if score.getScore() > record:
             record = score.getScore()
-            print(record)
         go.Over(bird, columns, score, record)
 
 if __name__ == '__main__':
