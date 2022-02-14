c448db60559b11f5a9a94c42c426b59ea108554a
ZinhNguyen <67100275+ZinhNguyen@users.noreply.github.com>
2022-02-14 17:40:40 +0700

    update main

--- main.py
+++ main.py
@@ -1,55 +1,39 @@
 # Import library
 import pygame
-from Image import bird as b, score as cs
-from Image import column as cl
-from Level import loading as lo
-from Level import start as st
-from Level import gameover as go
-from Level import characterChoice as cc
-from Image import arrow as ar
-from Image import logo as lg
-from Image import mcreen as ms
-from Image import background as bg
-from Image import content as ct
-from Utils import constant as const
-CHARACTER_SELECT_FONT_URL = const.CHARACTER_SELECT_FONT_URL
-CHARACTER_SELECT_SIZE = const.CHARACTER_SELECT_SIZE
-GUIDEFONT_URL = const.GUIDEFONT_URL
-GUIDEFONT_SIZE = const.GUIDEFONT_SIZE
-WHITE_COLOR = const.WHITE_COLOR
-BLACK_COLOR = const.BLACK_COLOR
+from Image import bird as b, score as sc
+from Image import column as cl, arrow as ar, logo as lg, mcreen as ms, background as bg, content as ct
+from Level import loading as lo, start as st, gameover as go, characterChoice as cc
 
+# Set init for pygame
 pygame.init()
-
+# Set title
+pygame.display.set_caption('FLAPPY BIRD')
 
 def main():
+    """This is main function for game"""
     record = 0
     blueBird = b.BlueBird()
     brownBird = b.BrownBird()
     defaultBird = b.Bird()
     background = bg.Background()
     columns = cl.Columns()
-    score = cs.Score()
+    score = sc.Score()
     arrow = ar.Arrow()
     logo = lg.Logo()
     mscreen = ms.Mscreen()
-    CharSelContent = ct.Content(CHARACTER_SELECT_FONT_URL, CHARACTER_SELECT_SIZE, 'CHARACTER SELECT', WHITE_COLOR, 200)
-    GuideContent = ct.Content(GUIDEFONT_URL, GUIDEFONT_SIZE, 'Press "Space" to choose', BLACK_COLOR, 530)
-    GuideLoadingContent = ct.Content(GUIDEFONT_URL, GUIDEFONT_SIZE, 'Click or use "Space" to start', BLACK_COLOR, 530)
-
     while True:
-        temp = cc.CharacterChoice(blueBird, defaultBird, brownBird, arrow, logo, mscreen, background, CharSelContent, GuideContent)
+        temp = cc.CharacterChoice(blueBird, defaultBird, brownBird, arrow, logo, mscreen, background)
         if temp == 1:
             bird = blueBird
         elif temp == 2:
             bird = brownBird
         else:
             bird = defaultBird
-        lo.Loading(bird, mscreen, background, GuideLoadingContent)
+        lo.Loading(bird, mscreen, background)
         st.Start(bird, columns, score, mscreen, background)
         if score.getScore() > record:
             record = score.getScore()
-        go.Over(bird, columns, score, record, mscreen, background)
+        go.Gameover(bird, columns, score, record, mscreen, background)
 
 if __name__ == '__main__':
     main()
